aea90e61d51a7061e92fe116f3009b0c16e4bdce
Sarah Andrew <sarah.andrew@stu.ocsb.ca>
2023-04-25 01:53:30 +0000

    Completed code.

--- RecPow.swift
+++ RecPow.swift
@@ -11,6 +11,7 @@
 
 //  Copyright (c) 2023 Sarah. All rights reserved.
 import Foundation
+
 // Define input & output paths.
 let inputFile = "input.txt"
 let outputFile = "output.txt"
@@ -26,11 +27,11 @@ do {
 
     // Open output file for writing.
     guard let output = FileHandle(forWritingAtPath: outputFile) else {
-        print("Error: cannot access output file for opening.")
+        print("Error: cannot access output file for writing.")
         exit(1)
     }
 
-    // Read context for file.
+    // Read content from file.
     let inputData = input.readDataToEndOfFile()
 
     // Convert data to a string.
@@ -39,70 +40,79 @@ do {
         exit(1)
     }
 
-    // Split string by newlines
+    // Split string by newlines.
     let inputLines = inputString.components(separatedBy: .newlines)
 
-    // Convert from string of list to array of integers.
-    // Creates a 2D array.
+    // Create  2D an array.
     var intLists: [[Int]] = []
 
-    // Usage of loop to access each element,
+    // Loop through each line of input.
     for line in inputLines {
-        // Separating each element by spaces.
+        // Separate each line by spaces.
         let lineParts = line.components(separatedBy: " ")
 
-        // Defining empty list.
+        // Define empty list.
         var intList: [Int] = []
 
-        // Usage of loop to access each element,
-        // then adding value to list.
+        // Loop through each part of the line.
         for part in lineParts {
             if let intValue = Int(part) {
                 intList.append(intValue)
-            //} else {
-                //print("Please enter valid input.")
+            } else {
+                continue
             }
         }
 
+        // Add the list of integers to the array.
         intLists.append(intList)
     }
 
-    // Accessing each element.
+    // Loop through each list of integers.
     for intList in intLists {
+         
+        if intList.count < 2 {
+            // Display error message and skip this list.
+            print("Invalid entry.")
+            // Write to file.
+            let data5 = Data(("Invalid entry." + "\n").utf8)
+            output.write(data5)
+            continue
+        }
+
+        // Access at index 0 & 1.
         let base = intList[0]
         let exp = intList[1]
 
-        // If statement to determine negative inputs.
+        // Usage of if statement to determine
+        // positive and negative values.
         if base >= 1 && exp >= 0 {
-            // Call function.
+            // Calls the function.
             let recPower = recPow(base: base, exp: exp)
 
-            // Display results.
-            print("The number", base, terminator: "")
-            print("^", exp, terminator: "")
-            print(" is =", recPower)
+            // Display to user.
+            print("The number \(base)^\(exp) is = \(recPower)")
 
-            let data = Data("For following input, the power is = \(recPower)\n".utf8)
-            output.write(data)
-            
+            // Write results to the output file.
+            let data3 = Data(("The number \(base)^\(exp) is = \(recPower)" + "\n").utf8)
+            output.write(data3)
         } else {
-            // Displays error to user.
-            print("Please enter a positive number.")
-            let data2 = Data("Please enter a positive number.".utf8)
-            output.write(data2)
+            // Display error & writes to file.
+            print("Please enter positive values.")
+            let data4 = Data(("Invalid entry, positive values." + "\n").utf8)
+            output.write(data4)
         }
     }
 }
-// This function uses recursion to
-// calculate the power of a number.
+// This function calculates the power of a
+// number recursively.
 func recPow(base: Int, exp: Int) -> Int {
-    // Calculates a power to a num.
-    // Define base cases
+    // Defines base case.
     if exp == 0 {
-        // Return to main
+        // Return a 1, if raised to 0.
         return 1
     } else {
-        // Calls function recursively.
+        // Calls the function recursively & returns it.
         return base * recPow(base: base, exp: exp - 1)
     }
 }
+
